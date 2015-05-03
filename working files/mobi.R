3830b61b70bdddc9589b056f3ddc9935c6bf6411
Juan Manuel <jmvelasquezestrada@gmail.com>
2015-05-04 02:26:50 +0800

    Adding some code for the evaluation of PLS as a predictive tool for a more complex model, in this case the mobi model

--- working files/mobi.R
+++ working files/mobi.R
@@ -6,8 +6,11 @@ library("semPLS")
 #Load data
 data("mobi")
 
-#Filter data 
-data<-mobi[1:250,]
+#Prepare the random sampling
+set.seed(123)
+index=sample.int(dim(mobi)[1],250,replace=F)
+trainData=mobi[index[1:200],]
+holdData=mobi[index[201:250],]
 
 #Load Structural and Measurement Models
 data("ECSIsm")
@@ -29,11 +32,13 @@ plsWeights(ecsi)
 plsLoadings(ecsi)
 
 
+
 #-----------------------------------------simplePLS--------------------------------
 
 
 #Load our Algorithm
 source("./lib/simplePLS.R")
+source("./lib/graphUtils.R")
 
 #Transform models to simplePLS
 smMatrix<-ECSIsm
@@ -41,11 +46,43 @@ mmMatrix<-cbind(ECSImm,rep("R",nrow(ECSImm)))
 colnames(mmMatrix)<-c("latent","measurement","type")
 
 #Call PLS-PM Function
-plsModel<-simplePLS(data,smMatrix,mmMatrix)
+plsModel<-simplePLS(trainData,smMatrix,mmMatrix)
 
-#Path Coefficients
-plsModel$path_coef
+#Call Prediction Function
+predHold <- PLSpredict(plsModel,holdData)
 
-plsModel$weightDiff
+#Set the panels
+par(mfrow=c(2,2))
 
-plsModel$outer_weights
+#Find scales
+xmax<-ceiling(max(c(holdData[,"CUSL1"],
+                    holdData[,"CUSL1"],
+                    holdData[,"CUSL1"])))
+
+ymax<-ceiling(max(c(predHold$predictedMeasurements[,"CUSL1"],
+                    predHold$predictedMeasurements[,"CUSL1"],
+                    predHold$predictedMeasurements[,"CUSL1"])))
+
+#PLS: Actual vs Predicted (CUSL1)
+y<-predHold$predictedMeasurements[,"CUSL1"]
+x<-holdData[,"CUSL1"]
+z<-holdData[,"CUSL1"]#nnPredData[,"mv11"]
+title="Actual vs Predicted (CUSL1)"
+xlabel=paste("Mean: Act=",
+             signif(mean(x),digits=4),
+             "PLS=",
+             signif(mean(y),digits=4),
+             "NN=",
+             signif(mean(z),digits=4),
+             "\n SD: Act=",
+             signif(sd(x),digits=4),
+             "PLS=",
+             signif(sd(y),digits=4),
+             "NN=",
+             signif(sd(z),digits=4))
+ylabel="Predicted"
+graphScatterplot(x,y,z,title,xlabel,ylabel,xmax=xmax,ymax=ymax)
+points(x,z, col= "black")
+
+
+
