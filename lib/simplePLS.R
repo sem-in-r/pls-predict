3608cf1de422bf6a0959dbbcef4457886e89a28e
Nicholas Danks <nicholasdanks@hotmail.com>
2016-03-13 12:03:44 +0800

    Updated validatePredict Issue 5

--- lib/simplePLS.R
+++ lib/simplePLS.R
@@ -319,60 +319,64 @@ predictlm <- function(model,newData){
 
 
 #Function for Generating and Evaluating Out-of-sample predictions using 10-fold cross-validaiton
-validatePredict <- function(newData, smMatrix, mmMatrix, maxIt=300, stopCriterion=7){
+validatePredict <- function(newData, smMatrix, mmMatrix, maxIt=300, stopCriterion=7,noFolds=10){
   
   #Randomly shuffle the data
   newData <- newData[sample(nrow(newData)),]
   
   #Create 10 equally size folds
-  folds <- cut(seq(1,nrow(newData)),breaks=10,labels=FALSE)
+  folds <- cut(seq(1,nrow(newData)),breaks=noFolds,labels=FALSE)
   
   #Identify variables to be tested
   items <- mmMatrix[ which(mmMatrix[,3]=='R'), "measurement" ]
   
   # Initialize matrices for prediction metrics
-  # Initialize predRMSE
-  predRMSE <- matrix(,nrow=10,ncol=length(items),byrow =TRUE,dimnames = list(1:10,items))
+  # Initialize RMSE holders
+  totalRMSE <- matrix(,nrow=1,ncol=length(items),byrow =TRUE,dimnames = list(1,items))
+  predSSE <- matrix(,nrow=noFolds,ncol=length(items),byrow =TRUE,dimnames = list(1:noFolds,items))
   # Initialize predMAPE
-  predMAPE <- matrix(,nrow=10,ncol=length(items),byrow =TRUE,dimnames = list(1:10,items))
+  predSAPE <- matrix(,nrow=noFolds,ncol=length(items),byrow =TRUE,dimnames = list(1:noFolds,items))
+  totalMAPE <- matrix(,nrow=1,ncol=length(items),byrow =TRUE,dimnames = list(1,items))
   # Initialize predMAD
-  predMAD <- matrix(,nrow=10,ncol=length(items),byrow =TRUE,dimnames = list(1:10,items))
-  
+  predSAD <- matrix(,nrow=noFolds,ncol=length(items),byrow =TRUE,dimnames = list(1:noFolds,items))
+  totalMAD <- matrix(,nrow=1,ncol=length(items),byrow =TRUE,dimnames = list(1,items))
   
   
   #Perform 10 fold cross validation
-  for(i in 1:10){
+  for(i in 1:noFolds){
     #Segment your data by fold using the which() function 
     testIndexes <- which(folds==i,arr.ind=TRUE)
     testData <- newData[testIndexes, ]
     trainData <- newData[-testIndexes, ]
     
-    #Train the PLS model
-    modelHolder <- simplePLS(trainData,smMatrix, mmMatrix, maxIt, stopCriterion)
-    
-    #  Test the PLS model
-    testHolder <- PLSpredict(modelHolder, testData)
-    
+    #PLS model
+    testHolder <- PLSpredict(trainData,smMatrix, mmMatrix, maxIt, stopCriterion, testData)
     
     #Iterate over no of residuals columns
     for(j in 1:ncol(testHolder$residuals)){
       
       #Calculate RMSE
-      foldRMSE[i,j] <- sqrt( mean( (testHolder$residuals[,j])^2) )
+      predSSE[i,j] <- sum(testHolder$residuals[,j]^2) 
       
       #Calculate MAPE
-      foldMAPE[i,j] <- mean((abs((testHolder$newData[,j] - testHolder$predictedMeasurements[,j])/testHolder$newData[,j])))
+      predSAPE[i,j] <- sum((abs((testHolder$newData[,j] - testHolder$predictedMeasurements[,j])/testHolder$newData[,j])))
       
       #Calculate MAD
-      foldMAD[i,j] <- mean(abs(testHolder$residuals[,j] - mean(testHolder$residuals[,j])))
+      predSAD[i,j] <- sum(abs(testHolder$residuals[,j] - mean(testHolder$residuals[,j])))
     }
   }
 
-validateResults <- list(predRMSE = predRMSE, 
-                        predMAPE = predMAPE,
-                        predMAD = predMAD)
-return(validateResults)
-
-
+  #Final calculations 
+  denom <- noFolds * nrow(testHolder$residuals)
+  for (k in 1:length(items)) {
+    totalRMSE[,k] <- sqrt((sum(predSSE[,k]))/denom)
+    totalMAPE[,k] <- 100*(sum(predSAPE[,k])/denom)
+    totalMAD[,k] <- sum(predSAD[,k])/denom
+  }
   
+  validateResults <- list(totalRMSE = totalRMSE, 
+                        totalMAPE = totalMAPE,
+                        totalMAD = totalMAD)
+  return(validateResults)
+
 }
